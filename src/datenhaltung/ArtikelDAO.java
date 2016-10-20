9413ed7dba680f9dae16af8a282c4a6f968717c4
gianluca <gianluca.most@innoq.com>
2016-10-20 17:35:53 +0200

    DAOs initialized

--- src/datenhaltung/ArtikelDAO.java
+++ src/datenhaltung/ArtikelDAO.java
@@ -1,7 +1,13 @@
 package datenhaltung;
 
+import logik.Artikel;
+
+import java.util.Collection;
+
 /**
  * Created by gianlucamost on 20.10.16.
  */
 public interface ArtikelDAO {
+    public Collection<Artikel> laden();
+    public void speichern(Collection<Artikel> artikel);
 }
--- src/datenhaltung/KundeDAO.java
+++ src/datenhaltung/KundeDAO.java
@@ -1,7 +1,13 @@
 package datenhaltung;
 
+import logik.Kunde;
+
+import java.util.Collection;
+
 /**
  * Created by gianlucamost on 20.10.16.
  */
 public interface KundeDAO {
+    public Collection<Kunde> laden();
+    public void speichern(Collection<Kunde> kunden);
 }
